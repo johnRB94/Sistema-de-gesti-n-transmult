8b07fd158e3daa8436e93adeeadff61d7cbf47dc
SebastianHDC <alitriji.xyz@gmail.com>
2026-03-02 19:09:48 -0500

    Update ProductoRepository.java

--- Transmult_Proyecto_Final/inventario/src/main/java/com/transmult/inventario/Repository/ProductoRepository.java
+++ Transmult_Proyecto_Final/inventario/src/main/java/com/transmult/inventario/Repository/ProductoRepository.java
@@ -4,8 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.transmult.inventario.Model.Producto;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+   static List<Producto> findAllByOrderByIdDesc() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'findAllByOrderByIdDesc'");
+}
+   
     Optional<Producto> findByCodigo(String codigo);
+    
 }
