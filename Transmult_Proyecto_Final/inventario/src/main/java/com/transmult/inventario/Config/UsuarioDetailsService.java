7a37acc345e675f4ba37c05db64af67c90f451d2
SebastianHDC <alitriji.xyz@gmail.com>
2026-03-02 19:12:54 -0500

    Update UsuarioDetailsService.java

--- Transmult_Proyecto_Final/inventario/src/main/java/com/transmult/inventario/Config/UsuarioDetailsService.java
+++ Transmult_Proyecto_Final/inventario/src/main/java/com/transmult/inventario/Config/UsuarioDetailsService.java
@@ -6,26 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
-
 @Service("usuarioDetailsService")
 public class UsuarioDetailsService implements UserDetailsService {
 
-    
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     @Override
     public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+
         Usuario usuario = usuarioRepository.findByUsuario(nombreUsuario)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
 
-                System.out.println("ROL DEL USUARIO: " + usuario.getRol());
+        // 👇 ACTUALIZAR FECHA DE ÚLTIMO ACCESO
+        usuario.setUltimoAcceso(java.time.LocalDateTime.now());
+        usuarioRepository.save(usuario);
 
         return User.builder()
                 .username(usuario.getUsuario())
-                .password(usuario.getContraseña()) // No encriptado, usar NoOp en dev
-                .roles(usuario.getRol().trim().toUpperCase()) // Asegura que tenga formato correcto
+                .password(usuario.getContraseña())
+                .roles(usuario.getRol().trim().toUpperCase())
                 .build();
     }
-    
+
 }
